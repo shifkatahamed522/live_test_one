fac2956bf18d05c8e96f6a69c2487c8c3cdd3e95
shifkatahamed522 <shifkatahamed522@gmail.com>
2024-07-13 16:29:21 +0600

    sss

---
+++ task2.php
@@ -0,0 +1,10 @@
+<?php
+$num1 = 55;
+$num2 = 75;
+$sum = $num1 + $num2;
+
+// Print result
+echo "The result is: $sum"; 
+
+// Print result using printf
+printf("The result of %d and %d is: %d", $num1, $num2, $sum);
---
+++ task3.php
@@ -0,0 +1,8 @@
+<?php
+$age = 30;
+
+if ($age >= 18 && $age <65){
+    echo "Eligible";
+}else {
+    echo "Not Eligible";
+}
